9d29fa1e6623fcc2142cf9ae8a2628049e33ee07
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-07-29 11:18:17 +0800

    Add files via upload

---
+++ mcxz/index_files/dlinfo1.js
@@ -0,0 +1,8 @@
+xvn="PC";
+xvo="";
+xdt="";
+xlk="https://www.xingzhenghang.tk/mcxz/pc";
+betavn="PE";
+betavo="";
+betadt="";
+betalk="https://www.xingzhenghang.tk/mcxz/pe";
---
+++ mcxz/pe/pe_files/dlinfo1.js
@@ -0,0 +1,8 @@
+xvn="";
+xvo="";
+xdt="";
+xlk="https://www.lanzous.com/tp/i4xm4ud";
+betavn="";
+betavo="";
+betadt="";
+betalk="https://www.lanzous.com/tp/i4xm50j";
