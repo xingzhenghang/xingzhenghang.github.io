b2a5581eed59ad625466a996d40a666d95361a32
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-07-29 12:53:11 +0800

    Add files via upload

---
+++ mcxz/pc/pc_files/dlinfo1.js
@@ -0,0 +1,8 @@
+xvn="";
+xvo="";
+xdt="";
+xlk="https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.exe";
+betavn="";
+betavo="";
+betadt="";
+betalk="http://mcdl2.down.v0vcq.cn/modpack/mc/Minecraft1.12.2.zip";
