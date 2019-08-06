4394b256e1b6d3e36d6cbcad97e794d2875b65b9
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-08-06 17:29:42 +0800

    Add files via upload

--- blog/fixoffice.bat
+++ blog/fixoffice.bat
@@ -1,6 +1,7 @@
 fltmc>nul||cd/d %~dp0&&mshta vbscript:CreateObject("Shell.Application").ShellExecute("%~nx0","%1","","runas",1)(window.close)&&exit
 echo off
-echo 修复 Microsoft Office 激活问题
+echo fix Microsoft Office 
 echo by Xingzhenghang
-echo Y|cacls C:\Windows\System32\spp /T /E /G NT SERVICE\sppsvc:F
+cacls C:\Windows\System32\spp /T /E /G NT SERVICE\sppsvc:F
+echo Y|
 pause
