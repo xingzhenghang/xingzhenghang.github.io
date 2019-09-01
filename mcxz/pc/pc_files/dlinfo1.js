ea533ad5f937180eda7d249870f57dd1d98d52c1
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-09-01 00:39:58 -0500

    Delete dlinfo1.js

--- mcxz/pc/pc_files/dlinfo1.js
+++
@@ -1,9 +0,0 @@
-java="https://java.com/zh_CN/download/";
-xvn="";
-xvo="";
-xdt="";
-xlk="https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.exe";
-betavn="";
-betavo="";
-betadt="";
-betalk="http://mcdl2.down.v0vcq.cn/modpack/mc/Minecraft1.12.2.zip";
