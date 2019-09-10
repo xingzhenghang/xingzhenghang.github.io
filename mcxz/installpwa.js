d5ed012999e136f499d47c3746357c31839e6b50
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-09-10 18:05:02 -0500

    Update installpwa.js

--- mcxz/installpwa.js
+++ mcxz/installpwa.js
@@ -1,26 +1,13 @@
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
-// CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
-// CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
-// CODELAB: Log user response to prompt.
-deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js')
+            .then(function (registration) {
+                // 注册成功
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(function (err) {
+                // 注册失败:(
+                console.log('ServiceWorker registration failed: ', err);
+            });
     });
-
-// CODELAB: Add event listener for appinstalled event
-// CODELAB: Add event lis
-window.addEventListener('appinstalled', logAppInstalled);
-
-// CODELAB: Add code to log the event// CODELAB: Add code to l
-console.log('MC下载 was installed.', evt);
+}
