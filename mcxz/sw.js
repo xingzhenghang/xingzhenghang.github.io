63f3291522e06bdd4a11e7d3d4fe9c6e53a6017b
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-09-13 21:04:31 +0800

    Update sw.js

--- mcxz/sw.js
+++ mcxz/sw.js
@@ -1,55 +1,64 @@
-var CACHE_VERSION = 'sw_v8';
-var CACHE_FILES = [
-    '//www.xingzhenghang.tk/ui/css/mdui.min.css',
-    '//www.xingzhenghang.tk/js/mdui.js',
-    './index.html',
-    './pe/index.html'
-    './pc/index.html'
-    '/',
-    '/index.html',
-    '/pe',
-    '/pc',
-    '//www.xingzhenghang.tk/images/card.png',
-];
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_VERSION)
-          .then(cache => cache.addAll(CACHE_FILES)
-    ));
-});
+let dataCacheName = 'mc-download'
+let cacheName = 'minecraft-download'
+let filesToCache = [
+  '//www.xingzhenghang.tk/ui/css/mdui.min.css',
+  '//www.xingzhenghang.tk/js/mdui.js',
+  './index.html',
+  './pe/index.html'
+  './pc/index.html'
+  '/',
+  '/index.html',
+  '/pe',
+  '/pc',
+  '//www.xingzhenghang.tk/images/card.png'
+]
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function(keys){
-            return Promise.all(keys.map(function(key, i){
-                if(key !== CACHE_VERSION){
-                    return caches.delete(keys[i]);
-                }
-            }));
-        })
-    );
-});
+self.addEventListener('install', function (e) {
+  console.log('SW Install')
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('SW precaching')
+      return cache.addAll(filesToCache)
+    })
+  )
+  self.skipWaiting()
+})
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            if(response){
-                return response;
-            }
-            var requestToCache = event.request.clone();
-            return fetch(requestToCache).then(
-              function(response){
-                if(!response || response.status !== 200){
-                  return response;
-                }
-                var responseToCache = response.clone();
-                caches.open(CACHE_VERSION)
-                  .then(function(cache){
-                    cache.put(requestToCache, responseToCache);
-                  });
-                return response;
-              }
-            );
+self.addEventListener('activate', function (e) {
+  console.log('SW Activate')
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('SW Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+  return self.clients.claim()
+})
+
+self.addEventListener('fetch', function (e) {
+  console.log('SW Fetch', e.request.url)
+  // 如果数据相关的请求，需要请求更新缓存
+  let dataUrl = '/mockData/'
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      caches.open(dataCacheName).then(function (cache) {
+        return fetch(e.request).then(function (response){
+          cache.put(e.request.url, response.clone())
+          return response
+        }).catch(function () {
+          return caches.match(e.request)
         })
-    );
-});
+      })
+    )
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        return response || fetch(e.request)
+      })
+    )
+  }
+})
