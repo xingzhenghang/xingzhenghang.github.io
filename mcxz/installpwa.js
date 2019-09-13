d378864a916e09053510e8bf624ab1c2c8e91be2
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-09-13 22:59:15 +0800

    Update installpwa.js

--- mcxz/installpwa.js
+++ mcxz/installpwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(function (registration) {
                 // 注册成功
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
