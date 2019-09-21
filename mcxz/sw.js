f5c8e637dc795212c3a5a3212ad44a4b89c52f58
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2019-09-20 20:56:10 -0500

    Update sw.js

--- mcxz/sw.js
+++ mcxz/sw.js
@@ -1,44 +1,132 @@
-var cacheName = 'minecraft-download';
-var cacheFiles = [
-    '//www.xingzhenghang.tk/ui/css/mdui.min.css',
-    '//www.xingzhenghang.tk/js/mdui.js',  './index.html',
-    './pe/index.html'
-    './pc/index.html'
-    '/',
-    '/index.html',
-    '/pe',
-    '/pc',
-    '//www.xingzhenghang.tk/images/card.png'
+'use strict';
+const version = 'v20170505';
+const __DEVELOPMENT__ = false;
+const __DEBUG__ = false;
+const offlineResources = [
+  '//www.xingzhenghang.tk/ui/css/mdui.min.css',
+  '//www.xingzhenghang.tk/js/mdui.js',  './index.html',
+  './pe/index.html'
+  './pc/index.html'
+  '/',
+  '/index.html',
+  '/pe',
+  '/pc',
+  '//www.xingzhenghang.tk/images/card.png',
 ];
 
-// 监听install事件，安装完成后，进行文件缓存
-self.addEventListener('install', function (e) {
-    console.log('Service Worker 状态： install');
-    var cacheOpenPromise = caches.open(cacheName).then(function (cache) {
-        return cache.addAll(cacheFiles);
-    });
-    e.waitUntil(cacheOpenPromise);
-});
-self.addEventListener('fetch', function (e) {
-    // 如果有cache则直接返回，否则通过fetch请求
-    e.respondWith(
-        caches.match(e.request).then(function (cache) {
-            return cache || fetch(e.request);
-        }).catch(function (err) {
-            console.log(err);
-            return fetch(e.request);
-        })
-    );
-});
-self.addEventListener('activate', function (e) {
-    console.log('Service Worker 状态： activate');
-    var cachePromise = caches.keys().then(function (keys) {
-        return Promise.all(keys.map(function (key) {
-            if (key !== cacheName) {
-                return caches.delete(key);
-            }
-        }));
+function onInstall(event) {
+    log('install event in progress.');
+    event.waitUntil(updateStaticCache());
+    self.skipWaiting()
+}
+
+function updateStaticCache() {
+    return caches.open(cacheKey('/')).then((cache) => {
+        return cache.addAll(offlineResources)
+    }).then(() => {
+        log('installation complete!')
     })
-    e.waitUntil(cachePromise);
-    return self.clients.claim();
-});
+}
+
+function onFetch(event) {
+    const request = event.request;
+    if (shouldAlwaysFetch(request)) {
+        event.respondWith(networkedOrOffline(request));
+        return
+    }
+    if (shouldFetchAndCache(request)) {
+        event.respondWith(networkedOrCached(request));
+        return
+    }
+    event.respondWith(cachedOrNetworked(request))
+}
+
+function networkedOrCached(request) {
+    return networkedAndCache(request).catch(() => {
+        return cachedOrOffline(request)
+    })
+}
+
+function networkedAndCache(request) {
+    return fetch(request).then((response) => {
+        var copy = response.clone();
+        caches.open(cacheKey('resources')).then((cache) => {
+            cache.put(request, copy)
+        });
+        log("(network: cache write)", request.method, request.url);
+        return response
+    })
+}
+
+function cachedOrNetworked(request) {
+    return caches.match(request).then((response) => {
+        log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
+        return response || networkedAndCache(request).catch(() => {
+            return offlineResponse(request)
+        })
+    })
+}
+
+function networkedOrOffline(request) {
+    return fetch(request).then((response) => {
+        log('(network)', request.method, request.url);
+        return response
+    }).catch(() => {
+        return offlineResponse(request)
+    })
+}
+
+function cachedOrOffline(request) {
+    return caches.match(request).then((response) => {
+        return response || offlineResponse(request)
+    })
+}
+
+function offlineResponse(request) {
+    log('(offline)', request.method, request.url);
+        return caches.match('/')
+    }
+}
+
+function onActivate(event) {
+    log('activate event in progress.');
+    event.waitUntil(removeOldCache())
+}
+
+function removeOldCache() {
+    return caches.keys().then((keys) => {
+        return Promise.all(keys.filter((key) => {
+            return !key.startsWith(version)
+        }).map((key) => {
+            return caches.delete(key)
+        }))
+    }).then(() => {
+        log('removeOldCache completed.')
+    })
+}
+
+function cacheKey() {
+    return [version, ...arguments].join(':')
+}
+
+function log() {
+    if (developmentMode()) {
+        console.log("SW:", ...arguments)
+    }
+}
+
+function shouldAlwaysFetch(request) {
+    return __DEVELOPMENT__ || request.method !== 'GET' || ignoreFetch.some(regex => request.url.match(regex))
+}
+
+function shouldFetchAndCache(request) {
+    return ~request.headers.get('Accept').indexOf('text/html')
+}
+
+function developmentMode() {
+    return __DEVELOPMENT__ || __DEBUG__
+}
+log("Hello from ServiceWorker land!", version);
+self.addEventListener('install', onInstall);
+self.addEventListener('fetch', onFetch);
+self.addEventListener("activate", onActivate);
