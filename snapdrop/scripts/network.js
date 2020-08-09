203293c7ceec2318b718c74a873c50ee12e486fe
xingzhenghang <53161520+xingzhenghang@users.noreply.github.com>
2020-08-09 13:22:38 +0800

    Update network.js

--- snapdrop/scripts/network.js
+++ snapdrop/scripts/network.js
@@ -509,8 +509,8 @@ RTCPeer.config = {
     'iceServers': [{
         urls: 'stun:stun.l.google.com:19302'
     }, {
-        urls: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
+        urls: 'stun:stun.xten.com:3478'
+    },{
+        urls: 'stun:stun.voipbuster.com:19302'
     }]
 }
